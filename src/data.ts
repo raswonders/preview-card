3f28ae014214485e2da4ce5bad676015c6149a7a
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-14 19:28:22 +0200

    add product data

---
+++ src/data.ts
@@ -0,0 +1,16 @@
+const product = {
+  category: "Perfume",
+  name: "Gabrielle Essence Eau De Parfum",
+  description:
+    "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+  price: {
+    current: 149.99,
+    original: 169.99,
+  },
+  image: {
+    url: "/src/assets/chanel.png",
+    alt_text: "Gabrielle Essence Eau De Parfum bottle by CHANEL",
+  },
+};
+
+export default product;
