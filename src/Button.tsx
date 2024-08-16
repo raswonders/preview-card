99dc7e42c852f754a16567904b4d23489aac4dc4
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-16 16:34:13 +0200

    add focus state

--- src/Button.tsx
+++ src/Button.tsx
@@ -11,10 +11,9 @@ export function Button({ variant, children, onclick }: ButtonProps) {
   let styles = "";
   switch (variant) {
     case "cta":
-      styles =
-        "flex items-center justify-center gap-2 py-4 font-montserrat font-bold text-sm bg-deep-aquamarine hover:bg-deep-aquamarine-hover transition-colors ease-in-out duration-300 rounded-[0.5rem] text-pure-white";
-      break;
     default:
+      styles =
+        "flex items-center justify-center gap-2 py-4 font-montserrat font-bold text-sm bg-deep-aquamarine hover:bg-deep-aquamarine-hover focus:outline-deep-aquamarine focus:outline-offset-2 transition-colors ease-in-out duration-300 rounded-[0.5rem] text-pure-white";
       break;
   }
 
