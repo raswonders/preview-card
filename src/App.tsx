e72beb49d09e3b6a663043042a858fd7534c7f33
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-16 16:18:13 +0200

    make App responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Product } from "./Product";
 import product from "./data";
 
 function App() {
-  return <Product product={product} />;
+  return (
+    <div className="min-h-screen sm:flex sm:justify-center sm:items-center">
+      <Product product={product} />
+    </div>
+  );
 }
 
 export default App;
